0848307ea1d9a706556228a5756622646e7d4c91
pedroqtx2026-jpg <pedroqtx2026@gmail.com>
2026-01-10 17:57:39 -0300

    Refactor Flask routes and update return messages

--- app.py
+++ app.py
@@ -3,12 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def inicio():
-    return "<h1>Bienvenido a la web de Pedro Alcz</h1><p>Mi primera página web online</p>"
+def home():
+    return "Web de Pedro Alcz funcionando correctamente"
 
 @app.route("/pedro-alcz")
 def pedro():
-    return "<h2>Proyecto Desarrollo Web - Pedro Alcz</h2>"
-
-if __name__ == "__main__":
-    app.run()
+    return "Ruta personalizada Pedro Alcz"
